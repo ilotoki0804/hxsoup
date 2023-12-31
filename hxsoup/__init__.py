816a9f70b7ce345c241318119a7652fd5e7fe689
ilotoki0804 <ilotoki0804@gmail.com>
2023-12-31 15:56:44 +0900

    dev import 제거

--- hxsoup/__init__.py
+++ hxsoup/__init__.py
@@ -12,7 +12,7 @@ Various convenient features related to httpx and BeautifulSoup.
 
 __title__ = "hxsoup"
 __description__ = "Various convenient features related to httpx and BeautifulSoup."
-__version_info__ = (0, 1, 0)
+__version_info__ = (0, 2, 0)
 __version__ = str.join(".", map(str, __version_info__))
 __author__ = "ilotoki0804"
 __author_email__ = "ilotoki0804@gmail.com"
@@ -24,4 +24,3 @@ __github_project_name__ = __title__
 from .api import delete, get, head, options, patch, post, put, request, stream
 from .client import Client, AsyncClient
 from .souptools import SoupedResponse, SoupTools
-from . import dev
